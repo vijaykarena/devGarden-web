4dda2d0b3a680c81649bf4a415709ed645871374
vijay karena <vijaykarena360@gmail.com>
2026-08-08 22:34:55 +0530

    fetch chat & show

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { createSocketConnection } from "../utils/socket";
 import { useSelector } from "react-redux";
+import { BASE_URL } from "../utils/constants";
+import axios from "axios";
 
 const Chat = () => {
   const { targetUserId } = useParams();
@@ -10,6 +12,28 @@ const Chat = () => {
 
   const user = useSelector((store) => store.user);
   const userId = user?._id;
+
+  const fetchChatMessages = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/chat/" + targetUserId, {
+        withCredentials: true,
+      });
+      const chatMessages = res.data.messages.map((msg) => {
+        return {
+          firstName: msg?.senderId?.firstName,
+          lastName: msg?.senderId?.lastName,
+          text: msg?.text,
+        };
+      });
+      setMessages(chatMessages);
+    } catch (error) {
+      console.error("Error fetching chat messages:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchChatMessages();
+  }, []);
 
   useEffect(() => {
     if (!userId) return;
@@ -20,9 +44,9 @@ const Chat = () => {
       targetUserId,
     });
 
-    socket.on("receivedMessage", ({ firstName, text }) => {
-      console.log(firstName + " : " + text);
-      setMessages((messages) => [...messages, { firstName, text }]);
+    socket.on("receivedMessage", ({ firstName, lastName, text }) => {
+      console.log(firstName + " " + lastName + " : " + text);
+      setMessages((messages) => [...messages, { firstName, lastName, text }]);
     });
 
     return () => {
@@ -34,6 +58,7 @@ const Chat = () => {
     const socket = createSocketConnection();
     socket.emit("sendMessage", {
       firstName: user?.firstName,
+      lastName: user?.lastName,
       userId,
       targetUserId,
       text: newMessage,
@@ -43,45 +68,34 @@ const Chat = () => {
 
   return (
     <div className="w-full flex justify-center p-10">
-      <div className="bg-gray-600 shadow-md rounded-lg w-3/4">
-        <div className="p-4 border-b bg-gray-700 text-white rounded-t-lg flex justify-between items-center">
+      <div className="bg-gray-700 shadow-md rounded-lg w-3/4">
+        <div className="p-4 border-b bg-gray-800 text-white rounded-t-lg flex justify-between items-center">
           <p className="text-lg font-semibold">Target User</p>
         </div>
         <div id="chatbox" className="p-4 h-96 overflow-y-auto">
           {/* <!-- Chat messages will be displayed here --> */}
-          {messages.map((message, index) => (
+          {messages.map((msg, index) => (
             <div key={index}>
-              <div className="chat chat-start">
-                <div className="chat-image avatar">
+              <div
+                className={
+                  "chat " +
+                  (user.firstName === msg.firstName ? "chat-end" : "chat-start")
+                }
+              >
+                {/* <div className="chat-image avatar">
                   <div className="w-10 rounded-full">
                     <img
                       alt="Tailwind CSS chat bubble component"
                       src="https://img.daisyui.com/images/profile/demo/kenobee@192.webp"
                     />
                   </div>
-                </div>
+                </div> */}
                 <div className="chat-header">
-                  {message.firstName}
+                  {msg.firstName + " " + msg.lastName}
                   {/* <time className="text-xs opacity-50">12:45</time> */}
                 </div>
-                <div className="chat-bubble">{message.text}</div>
+                <div className="chat-bubble">{msg.text}</div>
                 {/* <div className="chat-footer opacity-50">Delivered</div> */}
-              </div>
-              <div className="chat chat-end">
-                <div className="chat-image avatar">
-                  <div className="w-10 rounded-full">
-                    <img
-                      alt="Tailwind CSS chat bubble component"
-                      src="https://img.daisyui.com/images/profile/demo/anakeen@192.webp"
-                    />
-                  </div>
-                </div>
-                <div className="chat-header">
-                  Anakin
-                  <time className="text-xs opacity-50">12:46</time>
-                </div>
-                <div className="chat-bubble">Yes!</div>
-                <div className="chat-footer opacity-50">Seen at 12:46</div>
               </div>
             </div>
           ))}
